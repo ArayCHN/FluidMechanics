5d7b110d269ecbbd57e1f623b9dbd833fd7bc85f
ArayCHN <1172094131@qq.com>
2017-11-27 21:14:17 -0800

    modified Stoke's Second Problem Nondimensionalize Factor

--- project2-NavierStokes/NS.m
+++ project2-NavierStokes/NS.m
@@ -1,5 +1,6 @@
-uy = @(y, t) ( real(exp( -sqrt(1i/500/16.1).*y + 500i*t )) ); % the velocity function u(y, t)
-y = linspace(0, 1000, 1000); % create a series of height
+%uy = @(y, t) ( real(exp( -sqrt(1i/500/16.1).*y + 500i*t )) ); % the velocity function u(y, t)
+uy = @(y, t)(exp( -2*pi.*y).*cos(500*t - 2*pi.*y)); % the velocity function u(y, t)
+y = linspace(0, 1.25, 1000); % create a series of height
 ks = [0, 0.003, 0.006, 0.008, 0.01]; 
 for k = ks
     u = uy(y, k); % calculate an array of velocity
@@ -8,3 +9,5 @@ for k = ks
 end
 title('The Velocity Profile at Different Time')
 legend('t = 0','t = 0.003','t = 0.006','t = 0.008','t = 0.01')
+xlim([-1 1]) % set x axis scale
+ylim([0 1.25])
--- project2-NavierStokes/Write_up/code/NS.m
+++ project2-NavierStokes/Write_up/code/NS.m
@@ -1,10 +1,13 @@
-uy = @(y, t) ( real(exp( -sqrt(1i*500/16.1).*y + 500i*t )) ); % the velocity function u(y, t)
-y = linspace(0, 2, 1000); % create a series of height
-for k = 0:4
-    u = uy(y, k);
+%uy = @(y, t) ( real(exp( -sqrt(1i/500/16.1).*y + 500i*t )) ); % the velocity function u(y, t)
+uy = @(y, t)(exp( -2*pi.*y).*cos(500*t - 2*pi.*y)); % the velocity function u(y, t)
+y = linspace(0, 1.25, 1000); % create a series of height
+ks = [0, 0.003, 0.006, 0.008, 0.01]; 
+for k = ks
+    u = uy(y, k); % calculate an array of velocity
     plot(u, y); % plot the velocity with respect to height
     hold on
 end
-axis equal
 title('The Velocity Profile at Different Time')
-legend('t = 0','t = 1','t = 2','t = 3','t = 4')
+legend('t = 0','t = 0.003','t = 0.006','t = 0.008','t = 0.01')
+xlim([-1 1]) % set x axis scale
+ylim([0 1.25])
